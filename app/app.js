b7a690891399cf4a114ae70e45ba5a85a76df54e
Bruce Huang 黃耀進 <12061117@cecdm.com.tw>
2018-07-09 13:31:32 +0800

    koa+es6 init

---
+++ app/app.js
@@ -0,0 +1,101 @@
+'use strict';
+
+var _regenerator = require('babel-runtime/regenerator');
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
+var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Koa = require('koa');
+var app = new Koa();
+
+// x-response-time
+
+app.use(function () {
+    var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(ctx, next) {
+        var start, ms;
+        return _regenerator2.default.wrap(function _callee$(_context) {
+            while (1) {
+                switch (_context.prev = _context.next) {
+                    case 0:
+                        start = Date.now();
+                        _context.next = 3;
+                        return next();
+
+                    case 3:
+                        ms = Date.now() - start;
+
+                        ctx.set('X-Response-Time', ms + 'ms');
+
+                    case 5:
+                    case 'end':
+                        return _context.stop();
+                }
+            }
+        }, _callee, undefined);
+    }));
+
+    return function (_x, _x2) {
+        return _ref.apply(this, arguments);
+    };
+}());
+
+// logger
+
+app.use(function () {
+    var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(ctx, next) {
+        var start, ms;
+        return _regenerator2.default.wrap(function _callee2$(_context2) {
+            while (1) {
+                switch (_context2.prev = _context2.next) {
+                    case 0:
+                        start = Date.now();
+                        _context2.next = 3;
+                        return next();
+
+                    case 3:
+                        ms = Date.now() - start;
+
+                        console.log(ctx.method + ' ' + ctx.url + ' - ' + ms);
+
+                    case 5:
+                    case 'end':
+                        return _context2.stop();
+                }
+            }
+        }, _callee2, undefined);
+    }));
+
+    return function (_x3, _x4) {
+        return _ref2.apply(this, arguments);
+    };
+}());
+
+// response
+
+app.use(function () {
+    var _ref3 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee3(ctx) {
+        return _regenerator2.default.wrap(function _callee3$(_context3) {
+            while (1) {
+                switch (_context3.prev = _context3.next) {
+                    case 0:
+                        ctx.body = 'Hello World';
+
+                    case 1:
+                    case 'end':
+                        return _context3.stop();
+                }
+            }
+        }, _callee3, undefined);
+    }));
+
+    return function (_x5) {
+        return _ref3.apply(this, arguments);
+    };
+}());
+
+app.listen(8080);
